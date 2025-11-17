8bea51950010802bd59957ea644ba503686dff16
sweta <sg060479@gmail.com>
2025-11-17 12:02:16 +0530

    react intro

---
+++ my-app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+
+  // <div>
+  //   <h1>my fist app react</h1>
+  //   <p>paragraph tag </p>
+  // </div>
+  <>
+  <h1>
+    my firat app
+  </h1>
+  <p>para</p>
+
+
+  </>
+  
+ 
+);
+
+
